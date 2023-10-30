11e037dbb1d23ad341449745ce9da174a1e65971
Moh Bareche <mbareche.bic@gmail.com>
2023-10-30 00:03:23 -0400

    server final commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+    cors({
+        origin: ["https://moh-app.netlify.app", "http://localhost:5173"],
+        methods: ["POST", "GET"],
+        credentials: true,
+    })
+);
+
+app.use("/auth", require("./routes/user.route"));
+app.use("/customer", require("./routes/customer.route"));
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log("Connecté a la base de données avec succes...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server démarré sur le port ${PORT}...`));
